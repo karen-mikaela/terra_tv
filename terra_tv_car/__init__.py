fd2d88da70ed9d02a65a67b79df239a3833f9c34
karen-mikaela <karen.mikaela@gmail.com>
2014-05-13 21:26:59 -0300

    added settings to upload

--- terra_tv_car/__init__.py
+++ terra_tv_car/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import os
 from flask.ext.mongoengine import MongoEngine
+
+SETTINGS_PATH = os.path.abspath(os.path.realpath(__file__))
+ROOT_PATH = os.path.abspath(os.path.join(SETTINGS_PATH, "../../"))
+UPLOAD_FOLDER = os.path.join(ROOT_PATH, "terra_tv_car/photos/")
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {'DB': "terra_car"}
 app.config["SECRET_KEY"] = "mys3cr3t3k3y"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 db = MongoEngine(app)
 
@@ -15,4 +24,3 @@ register_blueprints(app)
 
 if __name__ == '__main__':
     app.run()
-
