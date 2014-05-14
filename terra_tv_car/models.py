58b983e3c303e4f923f5dc1d1766f21072b1dc12
karen-mikaela <karen.mikaela@gmail.com>
2014-05-13 21:29:32 -0300

    deleting unnecessary information, added url to photo

--- terra_tv_car/models.py
+++ terra_tv_car/models.py
@@ -4,21 +4,19 @@ from terra_tv_car import db
 
 
 class Car(db.Document):
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     year = db.StringField(max_length=4, required=True)
     model = db.StringField(max_length=100, required=True)
     manufacturer  = db.StringField(max_length=10, required=True)
     photo  = db.StringField(max_length=100, required=True)
-    slug = db.StringField(max_length=255, required=True)
-
-    def get_absolute_url(self):
-        return url_for('car', kwargs={"slug ": self.slug})
 
     def __unicode__(self):
         return self.model
 
+    def get_photo_url(self):
+        return url_for('cars.photos', filename=self.photo)
+
     meta = {
         'allow_inheritance': True,
-        'indexes': ['-created_at', 'slug'],
-        'ordering': ['-created_at']
+        'indexes': ['model'],
+        'ordering': ['model']
     }
