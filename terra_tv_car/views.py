77c94bba84eab1fc1b784b997ce367da07a4b442
karen-mikaela <karen.mikaela@gmail.com>
2014-05-13 21:32:55 -0300

    not using form_model

--- terra_tv_car/views.py
+++ terra_tv_car/views.py
@@ -1,18 +1,28 @@
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
-
-from flask.ext.mongoengine.wtf import model_form
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
 from terra_tv_car.models import Car
+from terra_tv_car import app
+from terra_tv_car import ALLOWED_EXTENSIONS
+
+import os, datetime
 
 cars = Blueprint('cars', __name__, template_folder='templates')
 
+def uploaded_file_name(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 class ListView(MethodView):
 
-    def get(self, slug):
-        if slug:
-            car = Car.objects.get_or_404(slug=slug)
-            print vars(car)
+    def get(self, id):
+        if id:
+            car = Car.objects.get_or_404(id=id)
             if car:
+                try:
+                    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], car.photo))
+                except:
+                    pass
                 car.delete()
             return redirect(url_for('cars.list'))
         else:
@@ -21,51 +31,72 @@ class ListView(MethodView):
 
 
 class DetailView(MethodView):
-    form = model_form(Car, exclude=['created_at'])
+
+    def verified_file(self,filename):
+        extension = filename.split('.', 1)[1]
+        allowed = '.' in filename and extension in ALLOWED_EXTENSIONS
+        return {"extension":extension, "allowed":allowed}
+
+    def upload_file(self, photo):
+        verified = self.verified_file(filename=photo.filename)
+        photo_name = "{0}.{1}".format(datetime.datetime.today().strftime("%Y%m%d%H%M%S"),
+             verified['extension'])
+        if verified['allowed']:
+            try:
+                photo.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(photo_name)))
+                return photo_name
+            except:
+                return False
 
 
 
-    def get_context(self, slug=None):
-        form_cls = model_form(Car, exclude=('created_at'))
-
-        if slug:
-            car = Car.objects.get_or_404(slug=slug)
-            if request.method == 'POST':
-                form = form_cls(request.form, inital=car._data)
-            else:
-                form = form_cls(obj=car)
+    def get(self,id):
+        if id:
+            car = Car.objects.get_or_404(id=id)
         else:
             car = Car()
-            form = form_cls(request.form)
 
         context = {
             "car": car,
-            "form": form,
-            "create": slug is None
+            "create": id is None
         }
-        return context
+
+        return render_template('cars/detail.html',**context)
 
 
-    def get(self, slug):
-        context = self.get_context(slug)
-        return render_template('cars/detail.html', **context)
+    def post(self, id):
+        model = request.form['model']
+        year = request.form['year']
+        manufacturer = request.form['manufacturer']
+        photo = request.files['photo']
+
+        if photo:
+            photo_name = self.upload_file(photo)
+        if id:
+            car = Car.objects.get_or_404(id=id)
+            car.model = model
+            car.year = year
+            car.manufacturer = manufacturer
+            if photo:
+                #remove old photo
+                try:
+                    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], car.photo))
+                except:
+                    pass
+                #update photo
+                car.photo = photo_name
+        else:
+            car = Car(model=model, year=year, photo=photo_name, manufacturer=manufacturer)
+        car.save()
+
+        return redirect(url_for('cars.list'))
 
 
-    def post(self, slug):
-        context = self.get_context(slug)
-        form = context.get('form')
-
-        if form.validate():
-            car = context.get('car')
-            form.populate_obj(car)
-            car.save()
-
-            return redirect(url_for('cars.list'))
-        return render_template('cars/list.html', **context)
 
 
 # Register the urls
-cars.add_url_rule('/', defaults={'slug': None}, view_func=ListView.as_view('list'))
-cars.add_url_rule('/delete/<slug>/', view_func=ListView.as_view('delete'))
-cars.add_url_rule('/<slug>/', view_func=DetailView.as_view('detail'))
-cars.add_url_rule('/create/', defaults={'slug': None}, view_func=DetailView.as_view('create'))
+cars.add_url_rule('/', defaults={'id': None}, view_func=ListView.as_view('list'))
+cars.add_url_rule('/delete/<id>/', view_func=ListView.as_view('delete'))
+cars.add_url_rule('/car/<id>/', view_func=DetailView.as_view('detail'))
+cars.add_url_rule('/photo/<filename>/', 'photos', view_func=uploaded_file_name)
+cars.add_url_rule('/create/', defaults={'id': None}, view_func=DetailView.as_view('create'))
